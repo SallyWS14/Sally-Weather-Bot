58bd8d72950c07325841d10f83b46b39a2927e22
Cleman25 <clementan25@gmail.com>
2022-10-13 16:50:45 -0700

    New Beginnings

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		let x = 2+2
+        await interaction.reply('Pong!' + x);
+	},
+};

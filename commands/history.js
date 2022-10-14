27234b2c39cfa1b71b99c2403a45af14b2d1876c
Clement Abel <clementan25@gmail.com>
2022-10-14 12:34:05 -0700

    Updated modules

--- commands/history.js
+++ commands/history.js
@@ -3,8 +3,23 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('history')
-		.setDescription('Get weather history!'),
+		.setDescription('Get weather history!')
+		.addStringOption(
+			option => option.setName('location')
+				.setDescription('Location to get weather history for').setRequired(true),
+		)
+		.addStringOption(
+			option => option.setName('Date')
+				.setDescription('Date to get weather history for').setRequired(true),
+		)
+		.addStringOption(
+			option => option.setName('Time')
+				.setDescription('Time to get weather history for').setRequired(true),
+		),
 	async execute(interaction) {
-        await interaction.reply({content: `Something goes here!`});
+		const location = interaction.options.getString('location');
+		const date = interaction.options.getString('Date');
+		const time = interaction.options.getString('Time');
+		await interaction.reply({ content: `Your location is ${location}` });
 	},
 };

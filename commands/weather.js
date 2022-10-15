8f47f67cd3eb4c03db2d7f69e0eb4b08f9d6b38e
ishikaubc <ishikaubc@gmail.com>
2022-10-15 16:54:33 -0700

    so far updates

--- commands/weather.js
+++ commands/weather.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const https = require('node:https');
+const https = require('https');
 const { API_KEY } = require('../config.json');
 
 module.exports = {
@@ -7,18 +7,27 @@ module.exports = {
 		.setName('weather')
 		.setDescription('Get current weather!'),
 	async execute(interaction) {
-        https.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={2de35d4c586e85adf3afcbd482bd52dd}')
-        , (res) => {
-            let data = '';
-                res.on('data', (chunk) => {
-                    data += chunk;
-                    
-                });
-                res.on('end', async () => {
-                    const body = JSON.parse(data);
-                    console.log(body); // print result;
-                    await interaction.reply({content: `The current weather ${body}`, ephemeral: true});
-                });
-        }
-    }
-}
+        https.get(`https://api.openweathermap.org/data/2.5/weather?lat=49.9&lon=-119.9&appid=2de35d4c586e85adf3afcbd482bd52dd&units=metric`);
+        //https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=imperial
+        let data = '';
+        let weather = '';
+        https.get(`https://api.openweathermap.org/data/2.5/weather?lat=49.9&lon=-119.9&appid=2de35d4c586e85adf3afcbd482bd52dd&units=metric`,(res) => {
+            res.on('data',async (d) => {
+                // process.stdout.write(d);
+                data += d;;
+            })
+            res.on('end', async () => {
+                const body = JSON.parse(data);
+                console.log(body);
+                weather = body.main.temp;
+                // process.stdout.write(body.weather);
+
+                await interaction.reply({content: `The current weather ${weather}`, ephemeral: true});
+                //await interaction.reply({content: `The current weather ${body.weather}`, ephemeral: true})
+            });  
+        }).on('error',(e) => {
+            console.error(e);
+        }).end();
+        //await interaction.reply({content: `The current weather ${weather}`, ephemeral: true});
+    },
+};

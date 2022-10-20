81f593ff36539d730566cc75f9a1bd88423694c4
Clement Abel <clementan25@gmail.com>
2022-10-19 21:08:48 -0700

    Saving

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,19 +1,19 @@
 /* eslint-disable indent */
 /* eslint-disable no-trailing-spaces */
-// const { triggers } = require('../storage/triggers.json');
-// let triggerWords = [];
+ const { triggers } = require('../storage/triggers.json');
+ let triggerWords = [];
 
-// for (let i = 0; i < triggers.length; i++) {
-//     for (let j = 0; j < triggers[i].length; j++) {
-//         triggerWords.push(triggers[i][j]);
-//     }
-// }
+ for (let i = 0; i < triggers.length; i++) {
+     for (let j = 0; j < triggers[i].length; j++) {
+         triggerWords.push(triggers[i][j]);
+     }
+ }
 
-// module.exports = {
-// 	name: 'messageCreate',
-// 	execute(message) {
-// 		// console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-//         // if (message.author.bot) return;
-//         // if (message.content.contains())
-// 	},
-// };
+ module.exports = {
+ 	name: 'messageCreate',
+ 	execute(message) {
+ 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+       if (message.author.bot) return;
+       if (message.content.contains())
+	},
+};
